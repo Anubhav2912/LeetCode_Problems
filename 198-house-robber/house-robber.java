de4644eaa0311be3d4e73f28ae9e1b65135bb042
Anubhav Kumar Singh <113932642+Anubhav2912@users.noreply.github.com>
2025-08-26 10:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (8.52%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1){
+            return nums[0];
+        }
+        int prev2 = 0;
+        int prev1 = nums[0];
+        int ans = 0;
+        for(int state = 2; state < n+1; state++){
+            int pick = nums[state - 1] + prev2;
+            int noPick = 0 + prev1;
+            ans = Math.max(pick, noPick);
+            prev2 = prev1;
+            prev1 = ans;
+        }
+        return ans;
+    }
+}
